b2857f9316bb29c582610583dd9bf9f3f737d680
teja596 <sp0661@gmail.com>
2022-04-15 14:32:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+'''
+the system works on p2p communication without third party interaction.
+removal of third party ensures extra level of security in the chat.
+'''
+
+from crypt import methods
+from flask import Flask,request,render_template,redirect,url_for
+import rsa
+import pickle
+from flask_mail import Mail,Message
+import cryptocode
+import json
+from utility import generate_code,share_key,send_message,share_key_back
 
